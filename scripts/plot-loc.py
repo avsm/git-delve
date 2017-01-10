e6c5b79d76ad93f6583980047dfc0c436726423d
Anil Madhavapeddy <anil@recoil.org>
2017-01-10 18:08:47 +0000

    add python plotting script

---
+++ scripts/plot-loc.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pylab as plt
+import csv
+
+import datetime
+
+times = []
+repos = {}
+fname='loc.txt'
+with open(fname, 'r') as csvfile:
+  r = csv.reader(csvfile, delimiter=' ', quotechar='"')
+  for row in r:
+    print row
+    if int(row[1]) not in times:
+      times.append(int(row[1]))
+    if row[0] not in repos.keys():
+      repos[row[0]] = []
+    repos[row[0]].append(int(row[2]))
+
+Y = []
+labels = []
+for key,value in repos.iteritems():
+  Y.append(value)
+  labels.append(key)
+
+plt.stackplot(times, *Y, baseline="zero", labels=labels)
+plt.title("MirageOS 3 lines of code")
+plt.legend(loc=(0.25,0.4))
+plt.tight_layout()
+plt.savefig("loc.pdf", format="pdf")
