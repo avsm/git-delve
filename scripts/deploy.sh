1e7d0f8a60ad9bfc2e45865cb97c54f1a7418bda
Anil Madhavapeddy <anil@recoil.org>
2017-01-12 16:31:57 +0000

    add a travis deployment script to gh-pages

---
+++ scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -ex
+
+docker pull avsm/git-delve
+eval `ssh-agent -s`
+chmod 600 deploy_git_delve
+ssh-add deploy_git_delve
+mkdir html
+for i in contribs.pdf commits.pdf loc.pdf files.txt; do
+  docker run avsm/git-delve cat scripts/$i > html/$i
+done
+cd html
+git init
+git checkout -b gh-pages
+git add *
+git commit -m sync -a
+git remote add origin git@github.com:avsm/git-delve
+git push -u origin +gh-pages
